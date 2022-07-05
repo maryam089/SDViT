9a6faa43daedbebc5d513be375c4f767eb0ab202
maryam089 <maraymshah089@gmail.com>
2022-07-05 23:34:06 +0400

    update

--- Grid_Search_sweep.sh
+++ Grid_Search_sweep.sh
@@ -6,7 +6,7 @@ do
         for command in delete_incomplete launch
 		    do
         python -m domainbed.scripts.sweep ${command} --data_dir=./domainbed/data/ \
-        --output_dir=./domainbed/PACS_Output/ERM_ViT_self_dist_DeiT/sweep_RB_loss_${lambda1}_KL_Div_Temperature_${lambda2} --command_launcher multi_gpu --algorithms ERM_ViT_self_dist_DeiT  \
+        --output_dir=./domainbed/PACS_Output/ERM_SDViT_DeiT/sweep_RB_loss_${lambda1}_KL_Div_Temperature_${lambda2} --command_launcher multi_gpu --algorithms ERM_SDViT_DeiT  \
         --single_test_envs  --datasets PACS  --n_hparams 1 --n_trials 3  \
         --hparams """{\"batch_size\":32,\"lr\":5e-05,\"resnet_dropout\":0.0,\"weight_decay\":0.0,\"RB_loss_weight\":$lambda1,\"KL_Div_Temperature\":$lambda2}"""
         done
