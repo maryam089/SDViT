9a3b5804660f8a525702fd390a21ddbee3ea8d03
maryam089 <maryamshah089@gmail.com>
2022-07-18 23:00:51 +0400

    update

--- Baseline_sweep.sh
+++ Baseline_sweep.sh
@@ -2,7 +2,7 @@
     for command in delete_incomplete launch
     do
     python -m domainbed.scripts.sweep ${command} --data_dir=/home/computervision1/DG_new_idea/domainbed/data/ \
-    --output_dir=./domainbed/PACS_Output/ERM_ViT/CVTSmall/  --command_launcher multi_gpu --algorithms ERM_ViT  \
+    --output_dir=./domainbed/PACS_Output/ERM_ViT/backbone_DeitSmall/  --command_launcher multi_gpu --algorithms ERM_ViT  \
     --single_test_envs  --datasets PACS  --n_hparams 1 --n_trials 3  \
-    --hparams """{\"backbone\":\"T2T14\",\"batch_size\":32,\"lr\":5e-05,\"resnet_dropout\":0.0,\"weight_decay\":0.0}"""
+    --hparams """{\"backbone\":\"DeitSmall\",\"batch_size\":32,\"lr\":5e-05,\"resnet_dropout\":0.0,\"weight_decay\":0.0}"""
     done
--- Grid_Search_sweep.sh
+++ Grid_Search_sweep.sh
@@ -6,7 +6,7 @@ do
         for command in delete_incomplete launch
 		do
         python -m domainbed.scripts.sweep ${command} --data_dir=/home/computervision1/DG_new_idea/domainbed/data/ \
-        --output_dir=./domainbed/PACS_Output/ERM_SDViT/sweep_RB_loss_${lambda1}_KL_Div_Temperature_${lambda2} --command_launcher multi_gpu --algorithms ERM_SDViT  \
+        --output_dir=./domainbed/PACS_Output/ERM_SDViT/backbone_DeitSmall/sweep_RB_loss_${lambda1}_KL_Div_Temperature_${lambda2} --command_launcher multi_gpu --algorithms ERM_SDViT  \
         --single_test_envs  --datasets PACS  --n_hparams 1 --n_trials 3  \
         --hparams """{\"backbone\":\"DeitSmall\",\"batch_size\":32,\"lr\":5e-05,\"resnet_dropout\":0.0,\"weight_decay\":0.0,\"RB_loss_weight\":$lambda1,\"KL_Div_Temperature\":$lambda2}"""
         done
